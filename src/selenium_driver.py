ff6543ab7e82c7ea87e70ff7ca245a1d0e21a02e
RoopjeetSingh <roopjeetsingh2004@gmail.com>
2024-08-31 16:18:14 -0400

    Add files

--- src/selenium_driver.py
+++ src/selenium_driver.py
@@ -1,4 +1,3 @@
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException, TimeoutException, StaleElementReferenceException, \
@@ -7,7 +6,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import threading
 import time
-from selenium.webdriver.common.keys import Keys
 import requests
 from io import BytesIO
 from collections import OrderedDict
